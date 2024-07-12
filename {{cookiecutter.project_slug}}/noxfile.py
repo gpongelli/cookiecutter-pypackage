b1cab937416dafc55190ffbc46fb5a01a7dba665
gpongelli <gpongelli@users.noreply.github.com>
2024-07-12 17:16:03 +0200

    feat: added trivy container scanner

--- {{cookiecutter.project_slug}}/noxfile.py
+++ {{cookiecutter.project_slug}}/noxfile.py
@@ -100,6 +100,7 @@ def update_license(session):
     _dot_files.extend(list(Path().glob('./.yamllint')))
     _dot_files.extend(list(Path().glob('./.pre-commit-config.yaml')))
     _dot_files.extend(list(Path().glob('./hadolint.yaml')))
+    _dot_files.extend(list(Path().glob('./trivy.yaml')))
     _dot_files.extend(list(Path().glob('./Dockerfile')))
     if _dot_files:
         session.run("poetry",
@@ -284,6 +285,26 @@ def container_build(session):
         external=True,
     )
 
+    # run trivy on local project
+    session.run(
+        "podman",
+        "run",
+        "-v",
+        "/var/run/docker.sock:/var/run/docker.sock",
+        "-v",
+        ".:/root/proj",
+        "-v",
+        "./trivy_cache:/root/.cache/",
+        "-v",
+        "./trivy.yaml:/root/trivy.yaml",
+        "aquasec/trivy:0.53.0",
+        "-c",
+        "/root/trivy.yaml",
+        "fs",
+        "/root/proj",
+        external=True,
+    )
+
     session.run(
         "podman",
         "build",
@@ -301,3 +322,35 @@ def container_build(session):
         ".",
         external=True,
     )
+
+    # save image as tar to be scanned by trivy
+    session.run(
+        "podman",
+        "save",
+        "-o",
+        f"{{ cookiecutter.project_slug }}-{__version__}.tar",
+        f"{{ cookiecutter.project_slug }}:{__version__}",
+        external=True,
+    )
+
+    # run trivy scan
+    session.run(
+        "podman",
+        "run",
+        "-v", "/var/run/docker.sock:/var/run/docker.sock",
+        "-v", ".:/root/proj",
+        "-v", "./trivy_cache:/root/.cache/",
+        "-v", "./trivy.yaml:/root/trivy.yaml",
+        "aquasec/trivy:0.53.0",
+        "-c", "/root/trivy.yaml",
+        "image",
+        "--input", f"/root/proj/{{ cookiecutter.project_slug }}-{__version__}.tar",
+        external=True,
+    )
+
+    os.remove(f"{{ cookiecutter.project_slug }}-{__version__}.tar")
+
+    # this works on downloaded images from dockerhub; only way to work on local built images is through tar file
+    # "podman run -v /var/run/docker.sock:/var/run/docker.sock -v ./trivy_cache:/root/.cache/
+    #   -v ./trivy.yaml:/root/trivy.yaml aquasec/trivy:0.53.0 -c /root/trivy.yaml
+    #   image {{ cookiecutter.dockerhub_username }}/{{ cookiecutter.project_slug }}:{__version__}"
